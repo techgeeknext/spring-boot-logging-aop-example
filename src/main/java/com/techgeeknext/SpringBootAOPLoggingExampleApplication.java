c922b5669f12f52ba160f3a9caedbbf56e13d176
TechGeekNeXt <techgeeknext@gmail.com>
2021-08-03 16:57:24 +0530

    techgeeknext

---
+++ src/main/java/com/techgeeknext/SpringBootAOPLoggingExampleApplication.java
@@ -0,0 +1,12 @@
+package com.techgeeknext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAOPLoggingExampleApplication
+{
+	public static void main(String[] args) {
+        SpringApplication.run(SpringBootAOPLoggingExampleApplication.class, args);
+    }
+}
